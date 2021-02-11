b198e871dd364bb7e2499ac5280f30b6fd80e9bb
Muhammad Nauman Raza <devraza@outlook.com>
2021-02-11 09:37:49 +0000

    Heroku Commit

--- data/db/build.sql
+++ data/db/build.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS server (
     GuildID integer PRIMARY KEY,
     Battles integer DEFAULT 0,
-    Trades integer DEFAULT 0
+    Trades integer DEFAULT 0,
+    Members integer DEFAULT 0
 );
